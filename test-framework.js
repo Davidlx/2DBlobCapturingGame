8c5f602c18ba829180e9c3f2e19477c878c0c0c0
Daiwei Liu <dai00021@gmail.com>
2016-03-17 23:04:12 +0800

    Initate the basic testing framework which can help us speed up the process

---
+++ test-framework.js
@@ -0,0 +1,15 @@
+// To Test a js class or a js function, you can use the provided functions in this framework
+// However, this framework can only run at Node.js Command Line Tool Enviornment
+// Also, you need to use "npm install unit.js" to set the enviornment for testing
+var test = require('unit.js');
+
+
+// To test a function, you need to be able to refer to that function
+// You can either copy and paste it here, or you can include that file which is recommonded
+
+// During the testing process, you need to write as many testing cases as you can untill you can not find more.
+// For each testing case, you need to use the function as below to assert it (which is a way to test it). More APIs are provided here: http://unitjs.com/guide/quickstart.html
+// test.assert(typeof 'foobar' == 'int');
+
+
+// Ideally, you need to test at least 5 cases against each functions. And you need to provide commonts for each test case.
